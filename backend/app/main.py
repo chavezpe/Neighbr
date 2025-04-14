78019fbab219588a3cbac472bc301c33220fd2db
Angel Chavez-Penate <chavezpe@usc.edu>
2025-04-14 09:33:49 -0700

    Documentation for the main app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-# from routes.query import router as query_router  # Import your query route
-from routes.upload import router as upload_router  # Import your upload route
-# from routes.admin import router as admin_router  # Import your admin route
+# from routes.query import router as query_router
+from routes.upload import router as upload_router
+# from routes.admin import router as admin_router
 
 
 """
 
 cd backend/app
-uvicorn main:app --reload
+uvicorn main:app --reload or uvicorn main:app --host 127.0.0.1 --port 8000
 
-http://127.0.0.1:8000/docs
+http://127.0.0.1:8000/api/v1/docs
 
 """
 
-
-app = FastAPI(title = "Smart Policy Assistant API")
+app = FastAPI(
+    title = "PolicyPal API",
+    description = "API for the Smart Policy Assistant",
+    version = "0.1.0",
+    openapi_url = "/api/v1/openapi.json",
+    docs_url = "/api/v1/docs",
+    redoc_url = "/api/v1/redoc",
+    )
 
 # Optional: Enable CORS in development mode
 app.add_middleware(
@@ -36,5 +41,5 @@ def read_root():
 
 # # Mount routers
 # app.include_router(query_router, prefix = "/query", tags = ["query"])
-app.include_router(upload_router, prefix="/upload", tags = ["upload"])
+app.include_router(upload_router, prefix = "/upload", tags = ["upload"])
 # app.include_router(admin_router, prefix="/admin", tags = ["admin"])
